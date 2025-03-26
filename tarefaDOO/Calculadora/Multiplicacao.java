1e222e76671bc60a7c36726e0a74a53eba55e5d2
rafaelCJ12 <149029533+rafaelCJ12@users.noreply.github.com>
2025-03-26 12:00:48 -0300

    Add files via upload

---
+++ tarefaDOO/Calculadora/Multiplicacao.java
@@ -0,0 +1,6 @@
+public class Multiplicacao implements Operacao {
+    public double resultado(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+    
+}
---
+++ tarefaDOO/Calculadora/Subtracao.java
@@ -0,0 +1,6 @@
+public class Subtracao implements Operacao{
+    public double resultado(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+    
+}
